f7209cf5f4c636ffe1f595c0b10440aff35d73f8
zhouzhongqing <zhouzhongqing>
2022-09-06 16:32:45 +0800

    feat:更新

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 
 - 2019年12月23日21:11:32 更新   ssr_update.py 已打包成exe无需安装环境可直接执行ssr_update.bat
 - 2020年10月31日15:38:31 更新  ssr_update_upgrade_v2.py 已打包成exe无需安装环境可直接执行ssr_update_upgrade_v2.exe
+
+- 2022年9月6日15:58:11 更新   更新获取帐号地址，进入ss_Update点击ip_1.bat或ip_2.bat可直接更新帐号。
--- ss_Update/ip_2.bat
+++ ss_Update/ip_2.bat
@@ -1,12 +1,19 @@
 @Echo Off
-Title 从coding云端更新 SS 配置文件
+Title 从GitHub云端更新 SSR 配置文件
 cd /d %~dp0
-set BackDir=..\..
-wget --ca-certificate=ca-bundle.crt -c https://coding.net/u/Alvin9999/p/ip/git/raw/master/ssconfig.txt
+wget --no-check-certificate https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/ssconfig.txt
+
+if exist ssconfig.txt goto startcopy
+echo ip更新失败，请试试另外一个
+pause
+exit
+:startcopy
+
 del "..\gui-config.json_backup"
 ren "..\gui-config.json"  gui-config.json_backup
-certutil -decode %~dp0ssconfig.txt %~dp0gui-config.json
+b64 -d ssconfig.txt gui-config.json
 copy /y "%~dp0gui-config.json" ..\gui-config.json
 del "%~dp0ssconfig.txt"
 del "%~dp0gui-config.json"
-ECHO.&ECHO.已更新SSR配置文件,请按任意键退出,并重启程序. &PAUSE >NUL 2>NUL
+ECHO.&ECHO.已更新SSR配置文件,请按回车键或空格键启动程序！ &PAUSE >NUL 2>NUL
+exit
